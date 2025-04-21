48dbd159a2251e6520618ef2aee8973b3c79be8b
rolysarmiento <sarmiento.rmsp@gmail.com>
2025-04-21 12:18:55 -0500

    header 2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
-import './Header.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
 import Logo from '../assets/img/logo.svg'
 import Person from '../assets/img/person.png'
 import { getUser } from '../helpers/getUser'
@@ -7,27 +8,47 @@ export default function Header() {
   let user = getUser()
   return (
     <>
-      <div className='bodyheader'>
-        <header className="containerHeader">
-          <div className="logo">
-            <img src={Logo} alt="Clínica San Felipe" />
-          </div>
-          <div className="user-info">
-            <img src={Person} className="icon-person" />
-            <div>
-              <p><span className='user'>Hola, </span> </p>
-              <p><strong> {user} </strong></p>
-            </div>
-            <div className="dropdown">
-              <button className="dropdown-toggle">▼</button>
-              <div className="dropdown-menu">
-                <a href="/" className="dropdown-item">Cerrar sesión</a>
-              </div>
-            </div>
+      <header className="col-12">
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">
+                    <img src={Logo} style={{width: '80px'}}  />
+                </a>
+                <div className="navbar-collapse justify-content-end" id="navbarScroll">
+                    <ul className="navbar-nav ">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                                    className="bi bi-bell-fill" viewBox="0 0 16 16">
+                                    <path
+                                        d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2m.995-14.901a1 1 0 1 0-1.99 0A5 5 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901" />
+                                </svg>
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">
+                                {/* <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
+                                    className="bi bi-person-fill" viewBox="0 0 16 16">
+                                    <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
+                                </svg> */}
+                                <img src={Person} style={{width: '30px'}} />
+                            </a>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"aria-expanded="false">
+                                Hola , <br/>
+                                {user}
+                            </a>
+                            <ul className="dropdown-menu">
+                                <li><a className="dropdown-item" href="#">Cerrar Session</a></li>
+                            </ul>
+                        </li>
+                    </ul>
 
-          </div>
-        </header>
-      </div>
+                </div>
+            </div>
+        </nav>
+    </header>
     </>
   )
 };
